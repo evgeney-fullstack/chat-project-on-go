f1ac719892a79d3454aaf3bdccd6a150e45c1232
evgeney-fullstack <evgeney.kovalev@mail.ru>
2026-08-09 10:06:40 +0300

    implement: load chat server config from env vars and .env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,16 @@
-package server
+package main
+
+import (
+	"log"
+
+	"github.com/evgeney-fullstack/chat-project-on-go/internal/config"
+)
+
+func main() {
+	// Загружаем конфигурацию из переменных окружения (или используем значения по умолчанию).
+	cfg, err := config.Load()
+	if err != nil { // Если произошла ошибка при загрузке конфигурации.
+		log.Fatalf("failed to load config: %v", err) // Выводим ошибку и завершаем программу с кодом 1.
+	}
+
+}
